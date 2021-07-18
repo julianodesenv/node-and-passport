9ecd7b6714a107ea7bae517fbfa46526aaba46bd
Juliano <julianodesenv@gmail.com>
2021-07-18 16:33:24 -0300

    auth basic

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const methodOverride = require('method-override');
+const passport = require('passport')
 const app = express();
 
+/** PASSPORT BASIC */
+passport.use(require('./src/auth/basic'))
+
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(passport.initialize())
 
-
+app.get('*', passport.authenticate('basic', { session: false }))
 require('./src/index')(app)
 
 app.listen(9000, () => {
