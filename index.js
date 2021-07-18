8b85124120cf00ebb2f03663feb5633f8b222e17
Juliano <julianodesenv@gmail.com>
2021-07-18 17:02:41 -0300

    add mongoose e template engine

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+const path =  require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const methodOverride = require('method-override');
 const passport = require('passport')
-const app = express();
+const mongoose = require('mongoose')
+const app = express()
 
 /** PASSPORT BASIC */
-passport.use(require('./src/auth/basic'))
+// passport.use(require('./src/auth/basic'))
+// app.get('*', passport.authenticate('basic', { session: false }))
 
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(passport.initialize())
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'src/view'))
 
-app.get('*', passport.authenticate('basic', { session: false }))
 require('./src/index')(app)
 
+mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true })
 app.listen(9000, () => {
     console.log('Express has been started');
 })
--- src/controller/main/main.js
+++ src/controller/main/main.js
@@ -1,3 +1,4 @@
 module.exports = (req, res) => {
-    return res.json({ msg: 'Hello from Express.js + Passport ' })
+    //return res.json({ msg: 'Hello from Express.js + Passport ' })
+    return res.render('main/index')
 }
